c63fd5f1a59c523203bd675988fe209fb3b13df3
Samir Perez <samir@nativeag.io>
2023-04-13 20:51:42 -0500

    added UIProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import "@/styles/globals.css";
-import { darkTheme } from "@/themes";
-import { ThemeProvider } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import type { AppProps } from "next/app";
+import { UIProvider } from '@/components/context/ui';
+import '@/styles/globals.css';
+import { darkTheme } from '@/themes';
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider theme={darkTheme}>
-			<CssBaseline />
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<UIProvider>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</UIProvider>
 	);
 }
